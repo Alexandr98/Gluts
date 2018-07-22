d1db22f21c0edeab953d08d8da11283ca477944e
Alexandr98 <alex.pryimak@mail.ru>
2018-07-23 00:24:35 +0300

    header,carts popup and shop list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { CartComponent } from './shared/cart/cart.component';
+import { MainContentComponent } from './shared/main-content/main-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CartComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule
