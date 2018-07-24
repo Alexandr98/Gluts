2a16b4bb3d9a97588bb677cbc596f3d9eeedb6f6
Alexandr98 <alex.pryimak@mail.ru>
2018-07-24 23:44:29 +0300

    markup main content and cart

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -8,16 +8,22 @@ import { splitClasses } from '../../../../node_modules/@angular/compiler';
 })
 export class HeaderComponent {
   @Input()
-  public inTrash: {}[];
+  public inTrash;
 
   public condition: boolean = true;
 
   public toggle(): void {
     this.condition = !this.condition;
   }
-
+  public findIndex(item: number) {
+    for( let i = 0; i < this.inTrash.length; i++) {
+      if ( this.inTrash[i].id === item ) {
+        this.inTrash.splice(i, 1);
+      }
+    }
+  }
   public removeCart(event: number): void {
-      this.inTrash.splice(event, 1);
+    this.findIndex(event);
       this.inTrash.push(event);
   }
 }
