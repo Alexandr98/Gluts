8c13bc7cde2f1400c21a91a16de7ef96da27d7ff
Alexandr Pryimak <sashaprim98@gmail.com>
2018-07-25 12:42:46 +0300

    resolve conflicts (#3)

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { splitClasses } from '../../../../node_modules/@angular/compiler';
 
 @Component({
   selector: 'app-header',
@@ -8,35 +7,15 @@ import { splitClasses } from '../../../../node_modules/@angular/compiler';
 })
 export class HeaderComponent {
   @Input()
-<<<<<<< HEAD
   public inTrash;
-=======
-  public inTrash: {}[];
->>>>>>> 559cdf51e578541f9e2e92e4b7a452b49d87726a
 
   public condition: boolean = true;
 
   public toggle(): void {
     this.condition = !this.condition;
-<<<<<<< HEAD
   }
-  public findIndex(item: number) {
-    for( let i = 0; i < this.inTrash.length; i++) {
-      if ( this.inTrash[i].id === item ) {
-        this.inTrash.splice(i, 1);
-      }
+  public removeCart(event): void {
+    this.inTrash = this.inTrash.filter(el => el.id !== event.id);
     }
   }
-  public removeCart(event: number): void {
-    this.findIndex(event);
-      this.inTrash.push(event);
-  }
-=======
-  }
 
-  public removeCart(event: number): void {
-      this.inTrash.splice(event, 1);
-      this.inTrash.push(event);
-  }
->>>>>>> 559cdf51e578541f9e2e92e4b7a452b49d87726a
-}
